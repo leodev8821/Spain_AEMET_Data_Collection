8e3128b3dac09f790e2734cdad8c2a07dd0e9a26
leodev8821 <jaime.leo.caicedo@gmail.com>
2025-04-06 14:39:45 +0200

    Added group_ema_codes. Needs implementation

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -3,11 +3,13 @@ from .obtain_ema_code import obtain_stations_EMA_code
 from .export_to_csv import data_to_csv
 from .date_validate import date_validation
 from .make_error_journal import build_journal
+from .group_ema_codes import group_ema_codes
 
 __all__ = [
     'historical_data',
     'obtain_stations_EMA_code',
     'data_to_csv',
     'date_validation',
-    'build_journal'
+    'build_journal',
+    'group_ema_codes'
 ]
